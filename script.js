590b1d37a3550fd893023416f6ed7e7f10760831
Ricardo Francé <ricardolfrance@gmail.com>
2022-01-17 15:22:38 -0300

    Movimentos cobrinha]

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,7 +22,29 @@ function criarCobrinha () {
     }
 }
 
+function iniciarJogo {
+    criarBG();
+    criarCobrinha();
 
-criarBG();
-criarCobrinha();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    le newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
 
